fd988cf94b66998509e777edf8185a6f35c97cdf
kongsakchai <ksakchai.dev@gmail.com>
2025-01-01 20:15:09 +0700

    :tada: feat: add fullscreen toggle function

--- src/lib/components/Controller.svelte
+++ src/lib/components/Controller.svelte
@@ -1,12 +1,28 @@
 <script lang="ts">
 	import LeftArrow from '$lib/icons/LeftArrow.svelte';
 	import RightArrow from '$lib/icons/RightArrow.svelte';
+	import Scale from '$lib/icons/Scale.svelte';
 
 	import { slideStore } from '$lib/store/slide.svelte.js';
+
+	const toggleFullScreen = () => {
+		const doc = document.documentElement;
+		if (!document.fullscreenElement) {
+			doc.requestFullscreen();
+		} else {
+			document.exitFullscreen();
+		}
+	};
 </script>
 
 <div class="fixed bottom-4 left-4">
 	<div class="flex items-center gap-2 rounded-lg border border-line bg-white p-2 text-secondary-text shadow-lg">
+		<button onclick={toggleFullScreen}>
+			<Scale />
+		</button>
+
+		<div class="vline"></div>
+
 		<button onclick={slideStore.prev}>
 			<LeftArrow />
 		</button>
